c61328050c058b06d5daaeb481e65d961656ce94
gufran <gufran.xpertsinfosoft@gmail.com>
2015-10-17 15:01:57 +0530

    First Commit

---
+++ app/src/main/java/com/appxperts/materialstudy17oct/MainActivity.java
@@ -0,0 +1,25 @@
+package com.appxperts.materialstudy17oct;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.crouton_check);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+
+}
